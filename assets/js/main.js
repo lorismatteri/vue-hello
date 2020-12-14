0369f1b48f5426c8028c6e2a40444300ea735ed0
Loris Matteri <loris.matteri93@gmail.com>
2020-12-14 16:15:14 +0100

    Titolo
    Inserito un h1 con messaggio utilizzando vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 //Istruzioni:
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+let app = new Vue ({
+    el: "#root",
+    data: {
+        message: "Che la forza sia con te!",
+        title: "red_center"
+    }
+})
+
 //Bonus: Aggiungere un’immagine presa anch’essa da un data
